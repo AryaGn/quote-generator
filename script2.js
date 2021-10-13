9ce9e6ef0807131081787b85ddad627c60066613
AryaGn <92050931+AryaGn@users.noreply.github.com>
2021-10-13 22:34:03 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,80 @@
+var quotes = [
+
+  {
+  quote: "Self belief and hard work will always earn you success",
+  source: "Virat Kohli",
+  citation: "",
+  year: "",
+  tags: "Inspiration"
+  },
+  {
+  quote: "Your time is limited,so don't waste it living someone else's life",
+  source: "Steve Jobs",
+  citation: "",
+  year: "",
+  tags: "Literature"
+  },
+  {
+  quote: "Those who don't believe in magic won't get it",
+  source: "Roald Dalh",
+  citation: "The Minpins",
+  year: 1991,
+  tags: "Children's Novel"
+  },
+  {
+  quote: "A day without sunshine is like, you know, night.",
+  source: "Steve Martin",
+  citation: "",
+  year: "",
+  tags: "Humour"
+  },
+  {
+  quote:  "It's better to be hated for what you are than being loved for what you are not",
+  source: "Andre Gide",
+  citation: "Autumn Leaves",
+  year: 1950,
+  tags: "Autobiography"
+  },
+  {
+  quote: "“It’s the possibility of having a dream come true that makes life interesting.”",
+  source: "Paulo Coelho",
+  citation: "The Alchemist",
+  year: 2005,
+  tags: "Novel"
+  },
+  {
+  quote: "“A friend may be waiting behind a stranger’s face.”",
+  source: "Maya Angelou",
+  citation: "A Letter to my daughter",
+  year: 2008,
+  tags: "Children's literature"
+  },
+  {
+  quote: "You'll never get anywhere if you go about what-iffing like that.",
+  source: "Roald Dahl",
+  citation: "Charlie and the Great Glass Elevator",
+  year: 1972,
+  tags: ""
+  },
+  {
+  quote:"You never know what worse luck your bad luck has saved you from.",
+  source: "Cormac McCarthy",
+  citation: "No Country for Old Men",
+  year: 2005,
+  tags: "Novel"
+  },
+  {
+  quote: "I have been bent and broken, but — I hope — into a better shape.",
+  source: "Charles Dickens",
+  citation: "Great Expectations",
+  year: 1861,
+  tags: "Novels"
+  },
+  {
+  quote: "And though she be but little, she is fierce.",
+  source: "William Shakespear",
+  citation: "A Midsummer Night’s Dream",
+  year: 1600,
+  tags: "Literature"
+  }
+];
